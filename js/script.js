c35531efc58bab249285e8ae1fc5aa85d6ceaddc
RobertoZeppilli <robzep171@gmail.com>
2021-05-27 15:41:02 +0200

    Vue Email List

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            indirizzi: []
+        },
+        mounted: function () {
+            for (var i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((result) => {
+                        this.indirizzi.push(result.data.response)
+                    })
+            }
+
+        }
+    }
+);
